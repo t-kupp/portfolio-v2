6d27a2ff12ea4c78fd835d7ebd9101dd82924301
t-kupp <kupper.thorge@gmail.com>
2025-02-03 17:54:48 +0100

    adjusted ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { PiCopyrightThin } from 'react-icons/pi';
 
 export default function Footer() {
   return (
-    <div className='fixed bottom-0 left-6 z-50 flex items-center gap-4 p-2 text-sm font-light tracking-tight opacity-100 2xl:left-7 2xl:p-3'>
+    <div className='fixed bottom-0 left-6 z-50 flex items-center gap-4 p-2 text-sm tracking-tight opacity-100 2xl:left-7 2xl:p-3'>
       <ThemeSwitch />
       <Copyright />
     </div>
@@ -12,7 +12,7 @@ export default function Footer() {
 
 function Copyright() {
   return (
-    <div className='flex items-center gap-1'>
+    <div className='flex items-center gap-1 font-light'>
       <PiCopyrightThin className='mt-[1px]' size={12} />{' '}
       <span className='text-spaced text-sm'>2025</span>Jan-Thorge Kupper
     </div>
